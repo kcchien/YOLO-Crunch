a77f85a32ef0d2a9f9afa146e65855f905afaf54
ANDREW CRATON <andrew.craton@gmail.com>
2018-04-18 16:40:36 -0500

    Delete map.sh

--- cfg/map.sh
+++
@@ -1,4 +0,0 @@
-cd ~/darknet-alexeyAB
-for ((k=0;k<80100;k+=100)); do
-	./darknet detector map ~/YOLO-Crunch-DEV/cfg/obj.data ~/YOLO-Crunch-DEV/cfg/yolo-crunch-vehicles.cfg ~/YOLO-Crunch-DEV/models/yolo-crunch-vehicles_$k.weights | grep "IoU"
-done
